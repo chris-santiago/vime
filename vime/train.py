e41cf8932021068ffe1261e6d318cbf985988bed
chris-santiago <cjsantiago@gatech.edu>
2023-05-22 00:09:52 -0400

    refactor config

--- vime/train.py
+++ vime/train.py
@@ -4,8 +4,8 @@ import hydra
 @hydra.main(config_path="conf", config_name="config", version_base="1.3")
 def main(cfg):
     data = hydra.utils.instantiate(cfg.dataloader)
-    optim = hydra.utils.instantiate(cfg.optimizer)
-    model = hydra.utils.instantiate(cfg.model, optim=optim)
+    optim = hydra.utils.instantiate(cfg.model.optimizer)
+    model = hydra.utils.instantiate(cfg.model.nn, optim=optim)
     trainer = hydra.utils.instantiate(cfg.trainer)
     trainer.fit(model=model, datamodule=data)
 
