41160934c8cb1379fefdee27cb2d2df2c244018d
chris-santiago <cjsantiago@gatech.edu>
2023-05-30 00:10:27 -0400

    prelim vime comps

--- vime/__init__.py
+++ vime/__init__.py
@@ -1,12 +1,12 @@
 """Package initialization."""
 from importlib.metadata import version
 
-from _constants import Constants
-from models.encoder import VimeEncoder
-from models.mlp import VimeMLP
-from models.semi import VimeLearner
-
-from data import get_mnist_test, get_mnist_train
+from ._constants import Constants
+from .data import get_mnist_test, get_mnist_train
+from .models.encoder import VimeEncoder
+from .models.mlp import VimeMLP
+from .models.semi import VimeLearner
+from .utils import instantiate_callbacks
 
 __version__ = version("vime")
 __all__ = [
@@ -16,4 +16,5 @@ __all__ = [
     VimeLearner,
     get_mnist_test,
     get_mnist_train,
+    instantiate_callbacks,
 ]
